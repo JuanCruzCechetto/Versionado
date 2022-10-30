d828bf3158f00b457769ae618d6ec9dcea8e17fe
Juan Cruz Cechetto <juancruzcechetto03@gmail.com>
2022-10-30 16:00:02 -0300

    Tp integrador terminado

--- Ingra4CnStruck.c
+++ Ingra4CnStruck.c
@@ -1,29 +1,3 @@
-/*
-Se desea realizar un programa que gestione el stock de un negocio de venta de 
-electrodomésticos. Como máximo se permitirán cargar 100 electrodomésticos. De cada 
-electrodoméstico se conoce su código de barras, su descripción y una cantidad en stock. 
-Se deberá presentar un menú con las siguientes opciones:
-1. Cargar productos.
-2. Modificar stock.
-3. Mostrar productos sin stock.
-4. Listar productos.
-5. Salir.
-Donde, la opción 1 permite cargar los 100 electrodomésticos ingresando para cada uno: código 
-de barras, descripción y cantidad.
-La opción 2 debe pedir el código de barras del electrodoméstico al cual se desea cambiar el 
-stock y la nueva cantidad. Si el código de barras ingresado no existe, informar la situación con 
-un mensaje “Electrodoméstico inexistente”, si el código de barras si se encuentra modificar su 
-stock con la cantidad ingresada.
-La opción 3 debe mostrar en la consola los datos de aquellos electrodomésticos con cantidad 
-igual a cero.
-La opción 4 debe mostrar en la consola el listado de electrodomésticos.
-SOLO se saldrá de la aplicación si se selecciona la opción 5
- 
-Juan Cruz Cechetto
-Sara Guala
-Valentin Gimenez
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -62,7 +36,7 @@ void main()
             {
                 case 1: 
                     
-                    cargarProd(); //sub proceso
+                    cargarProd();
                     break;
                 case 2: 
                     
@@ -86,7 +60,7 @@ void main()
 
 
 }
-void cargarProd() //Subproceso=Pide datos y va guardando en c/arreglo // For se mueve hasta la cantidad de productos que tenga/quieras cargar
+void cargarProd()
 {
     printf("--Ingres%c los datos del Producto--\n", 138);
 
@@ -95,10 +69,10 @@ void cargarProd() //Subproceso=Pide datos y va guardando en c/arreglo // For se 
     {
         printf("C%cdigo del producto: ",162); 
         scanf("%i", &lista_producto[i].codigodeP);
-        fflush (stdin); //Limpia la consola
+        fflush (stdin); 
         printf("Ingrese la descripci%cn: ",162);
         printf("\n");
-        scanf("%[^\n]"/*Deja ingresar palabras con espacio*/, &lista_producto[i].descripcion);
+        scanf("%[^\n]", &lista_producto[i].descripcion);
         
         
         printf("Ingrese la cantidad: ");
@@ -107,7 +81,7 @@ void cargarProd() //Subproceso=Pide datos y va guardando en c/arreglo // For se 
         
     }
 }
-void modificarStockdeProductos() // Pide un Cdebarra se fija si esta en la lista de codigos.
+void modificarStockdeProductos()
 {
     int codB = 0, cant = 0;
     int encontrado = 0;
@@ -115,11 +89,11 @@ void modificarStockdeProductos() // Pide un Cdebarra se fija si esta en la lista
     printf("Ingrese el C%cdigo del producto para modificar su stock: ", 162); 
     scanf("%i", &codB);
 
-    for(int i = 0; i < tam; i++) //recorre todos los productos y nos permite modificar el stock cuando encuentra el mismo codigo de barra
+    for(int i = 0; i < tam; i++) 
 
     {
      
-        if(codB == lista_producto[i].codigodeP) // Si el Cod esta en la lista pide que ingrese nuevo stock
+        if(codB == lista_producto[i].codigodeP)
         {
             printf("\n1- Ingrese nuevo stock: \n", lista_producto[i].codigodeP);
             printf("\n");
@@ -135,23 +109,23 @@ void modificarStockdeProductos() // Pide un Cdebarra se fija si esta en la lista
         printf("El electrodom%cstico es inexistente...\n", 138);  
     }
 }
-void mostrarProductossinStock() //Recorre los productos y si alguno es igual a 0 (sin stock) se muestra
+void mostrarProductossinStock()
 {  
     printf("Lista de electrodomesticos sin stock\n");
            
             for (int i = 0; i < tam ; i++)
             {
                 
-                if(lista_producto[i].cantidad ==0) //controla si producto tiene stock 0  
+                if(lista_producto[i].cantidad ==0) 
                 {
                     printf("Codigo del producto sin stock es: %i \n", lista_producto[i].codigodeP);
                     
                 }
-                //Mejora: hacer un IF fuera del for pára la variable encontrada
+                
             }                                    
         
 }         
-void listadoProductos() //recorre todo el arreglo y muestra toda la informacion de todos los produco
+void listadoProductos() 
 {
     printf("Listado de productos: \n");
 
